6b17a85d7306c248afb13ca68aa89497ff6c18fd
Link184 <pacific.trac@gmail.com>
2020-02-06 16:03:46 +0200

    Changed maven repo

--- sample/src/main/java/com/daasuu/gpuvideoandroid/PlayerActivity.java
+++ sample/src/main/java/com/daasuu/gpuvideoandroid/PlayerActivity.java
@@ -4,10 +4,16 @@ import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.*;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.daasuu.gpuv.egl.filter.GlFilter;
 import com.daasuu.gpuv.player.GPUPlayerView;
@@ -116,9 +122,11 @@ public class PlayerActivity extends AppCompatActivity {
         });
 
         filterSeekBar = (SeekBar) findViewById(R.id.filterSeekBar);
+        TextView tempText = findViewById(R.id.tempText);
         filterSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                tempText.setText("Filter config: " + progress);
                 if (adjuster != null) {
                     adjuster.adjust(filter, progress);
                 }
@@ -145,6 +153,8 @@ public class PlayerActivity extends AppCompatActivity {
                 filter = FilterType.createGlFilter(filterTypes.get(position), getApplicationContext());
                 adjuster = FilterType.createFilterAdjuster(filterTypes.get(position));
                 findViewById(R.id.filterSeekBarLayout).setVisibility(adjuster != null ? View.VISIBLE : View.GONE);
+                TextView tempText = findViewById(R.id.tempText);
+                tempText.setText("");
                 gpuPlayerView.setGlFilter(filter);
             }
         });
